c48cb94de35450758ef46fa6d4f14d5c8c64391d
Денис Бессалый <bessalyi.d@rusrobors.ru>
2022-01-14 01:28:47 +0300

    Исправлено название метода.

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -13,7 +13,7 @@ namespace JfService.Balance.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<JfServiceDbContext>();
 

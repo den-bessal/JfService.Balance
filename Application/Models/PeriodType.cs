901d47a36f683a162b3117301c16aa2d855b1a3b
Денис Бессалый <bessalyi.d@rusrobors.ru>
2022-01-14 03:11:12 +0300

    Реализация возможности вывода читаемых значений перечисления PeriodType в ответе на запрос.

--- Application/Models/PeriodType.cs
+++ Application/Models/PeriodType.cs
@@ -1,23 +1,31 @@
-﻿namespace JfService.Balance.Application.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace JfService.Balance.Application.Models
 {
     /// <summary>
     /// Тип периода.
     /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum PeriodType
     {
         /// <summary>
         /// Год.
         /// </summary>
+        [EnumMember(Value = "Год")]
         Year,
 
         /// <summary>
         /// Квартал.
         /// </summary>
+        [EnumMember(Value = "Квартал")]
         Quarter,
 
         /// <summary>
         /// Месяц.
         /// </summary>
+        [EnumMember(Value = "Месяц")]
         Month
     }
 }
